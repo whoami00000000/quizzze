55536696b8eac60d2f530988eeff73cfed1c3bc5
Sergey <sayvix005@gmail.com>
2024-11-26 20:48:21 +0300

    add block

--- app/ui/UserBlocked.tsx
+++ app/ui/UserBlocked.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import WebApp from '@twa-dev/sdk';
 import { UserDataTg } from '../lib/definitions';
+import { FaExclamationTriangle } from 'react-icons/fa';
 
 const UserBlockedNotification = () => {
   const [isBlocked, setIsBlocked] = useState(false);
@@ -64,9 +65,15 @@ const UserBlockedNotification = () => {
             transition={{ duration: 0.5, ease: 'easeOut', delay: 0.3 }}
             className="bg-white rounded-lg shadow-2xl p-8 text-center max-w-md"
           >
+            <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-red-600 text-white rounded-full p-4">
+              <FaExclamationTriangle className="text-4xl animate-bounce" />
+            </div>
             <h2 className="text-3xl font-extrabold text-red-600 mb-4">Доступ ограничен</h2>
             <p className="text-lg text-gray-800 mb-6">
               Ваш аккаунт заблокирован. Обратитесь в поддержку, если считаете это ошибкой.
+            </p>
+            <p className="text-lg text-gray-800 mb-6">
+              Малик сосаааааать
             </p>
           </motion.div>
         </motion.div>
