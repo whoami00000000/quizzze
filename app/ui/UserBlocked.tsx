be20e726613c491816fa51aa3a72ebfe6347edcb
Sergey <sayvix005@gmail.com>
2024-11-26 20:53:10 +0300

    add block

--- app/ui/UserBlocked.tsx
+++ app/ui/UserBlocked.tsx
@@ -69,7 +69,7 @@ const UserBlockedNotification = () => {
               <FaExclamationTriangle className="text-4xl animate-bounce" />
             </div>
             <h2 className="text-3xl font-extrabold text-red-600 mb-4">Доступ ограничен</h2>
-            <p className="text-lg text-gray-800 mb-6">
+            <p className="text-xs text-gray-400 mb-6">
               Ваш аккаунт заблокирован. Обратитесь в поддержку, если считаете это ошибкой.
             </p>
             <p className="text-lg text-gray-800 mb-6">
